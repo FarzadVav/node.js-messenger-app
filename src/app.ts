2e8a5f03b97c1556bd30c6b9a352520f46965d95
FarzadVav <farzad.vav.work@gmail.com>
2025-09-01 17:16:14 +0330

    fix typescript

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
+
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(req);
   res.send("Hello World!");
 });
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
